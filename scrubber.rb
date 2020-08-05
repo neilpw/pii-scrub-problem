9aa68a6e2f8a798d394cab0494183ee0456b8b3c
Neil Banerjee <neil.m.banerjee@gmail.com>
2020-08-05 16:53:48 -0700

    Scrubber: Introduce #scrub_value.
    For now, this doesn't actually scrub anything. But it does blow up on invalid data.

--- scrubber.rb
+++ scrubber.rb
@@ -18,6 +18,31 @@ class Scrubber
   def scrub(input)
     parsed = Oj.load(input)
 
-    Oj.dump(parsed)
+    scrubbed = scrub_value(parsed)
+
+    Oj.dump(scrubbed)
+  end
+
+  # Given any valid JSON type, scrub it for sensitive data.
+  #
+  def scrub_value(input)
+    # It would have been nice to make this a case statement, but I don't think
+    # that would play nicely with the various Numeric types.
+    #
+    if input.is_a?(Hash)
+      input # not yet supported
+    elsif input.is_a?(Array)
+      input # not yet supported
+    elsif input.is_a?(String)
+      input # not yet supported
+    elsif input.is_a?(Numeric)
+      input # not yet supported
+    elsif input.is_a?(TrueClass) || input.is_a?(FalseClass)
+      input # not yet supported
+    elsif input.is_a?(NilClass)
+      input # not yet supported
+    else
+      raise "Invalid input type: #{input} <#{input.class}>"
+    end
   end
 end
